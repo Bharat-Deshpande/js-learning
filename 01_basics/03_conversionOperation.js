cc012f96fd72973311fd92df835586d60851931d
Bharat-Deshpande <149424252+Bharat-Deshpande@users.noreply.github.com>
2024-10-28 17:29:17 +0000

    added another file

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,7 +23,44 @@
 // "bharat" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ****************Operations********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2**2)
+
+// let str1 = "hello"
+// let str2 = "Bharat"
+
+
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(("1") + 2); // prints 12
+// console.log(1 + "2"); // prints 12
+// console.log("1"+2+2); // prints 122
+
+// console.log(1+2+"2"); // prints 32
+
+
+// console.log(+true) //prints 1
+
+// console.log(+"") // prints 0
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++ //101
+console.log(gameCounter++) //prints 101 and then increment
+console.log(gameCounter) // prints 102
---
+++ 01_basics/05_dataTypesSummary.js
@@ -0,0 +1,40 @@
+// primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outSideTemp = null
+// let userEmail; // undefined
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id === anotherId); //false
+
+// const bigNumber = 3434353223423n  //BigInt
+
+
+
+
+// refernce (Non primitive)
+// Arrays, Objects, functions
+
+
+const heros = ["shaktiman", "Bharat", "Mohit"] //array
+
+let myObj = {
+    name: "Bharat",   //object
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof heros); // object 
+console.log(typeof myObj); // object
+console.log(typeof myFunction); //function
+
+
