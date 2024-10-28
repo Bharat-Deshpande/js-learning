e14c49f0115c20efb8924aa7146773b3ebda05dc
Bharat-Deshpande <149424252+Bharat-Deshpande@users.noreply.github.com>
2024-10-28 17:37:51 +0000

    added a file

---
+++ 01_basics/06_stackAndHeap.js
@@ -0,0 +1,25 @@
+//stack => primitive
+// Heap => Non primitive
+
+
+let myname = "Bharat" // primitive so goes into stack
+
+
+let anotherName = myname
+anotherName = "Bharat Deshpande"
+console.log(myname); // Bharat
+console.log((anotherName)); // Bharat Deshpande
+
+
+let userOne = {
+    email: "bharat@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne;
+
+userTwo.email = "bharat@goodle"
+
+console.log(userOne.email); // bharat@goodle
+console.log(userTwo.email); //bharat@goodle
+
