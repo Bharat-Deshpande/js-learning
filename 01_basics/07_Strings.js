33cdd7459fb5f3c2a14cf4fcedae04754f9cfcba
Bharat-Deshpande <149424252+Bharat-Deshpande@users.noreply.github.com>
2024-10-29 15:18:07 +0000

    Added folders

---
+++ 01_basics/07_Strings.js
@@ -0,0 +1,42 @@
+// const name = "Bharat"
+// const repCount = 50
+
+// console.log(name + repCount+ " value"); // dont recommend
+
+// console.log(`Hello my name is ${name} and my repo account is ${repCount}`);
+
+const gameName = new String("BHarat-SD") //Strings are objects in javascript
+
+// console.log(gameName[0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStr1 = "  Bharat  "
+// console.log(newStr1.trim());
+
+const url = "https://Bharat.com/bharat%20deshpande"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes("bharat"));
+
+
+console.log(gameName.split("-"));
+console.log(gameName.split(""));
+
+
+
+
+
+
+
+
+
